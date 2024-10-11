3e649fedaf6f3001e095c73bd55e538be46eed66
HodeonArtz <martidicianolold@gmail.com>
2024-10-11 18:44:20 +0200

    BankAccountInterface

--- src/ComBank/Transactions/Contracts/BankTransactionInterface.php
+++ src/ComBank/Transactions/Contracts/BankTransactionInterface.php
@@ -9,8 +9,23 @@
 
 use ComBank\Bank\Contracts\BackAccountInterface;
 use ComBank\Exceptions\InvalidOverdraftFundsException;
+use ComBank\OverdraftStrategy\Contracts\OverdraftInterface;
 
 interface BankTransactionInterface
 {
+    public function transaction(BankTransactionInterface $transaction) : void;
     
+    public function openAccount() : void;
+
+    public function reopenAccount() : void;
+
+    public function closeAccount() : void;
+
+    public function getBalance() : float;
+
+    public function getOverdraft() : OverdraftInterface;
+
+    public function applyOverdraft(OverdraftInterface $overdraft) : void;
+
+    public function setBalance(float $balance) : void;
 }
