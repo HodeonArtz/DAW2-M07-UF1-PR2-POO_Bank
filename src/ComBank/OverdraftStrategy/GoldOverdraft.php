91a7430f0d7d44d581613287661c70d1ba2b0d49
HodeonArtz <martidicianolold@gmail.com>
2024-10-25 18:33:11 +0200

    Basic and Gold overdraft implemented

---
+++ src/ComBank/OverdraftStrategy/GoldOverdraft.php
@@ -0,0 +1,25 @@
+<?php namespace ComBank\OverdraftStrategy;
+      use ComBank\OverdraftStrategy\Contracts\OverdraftInterface;
+
+/**
+ * Created by VS Code.
+ * User: JPortugal
+ * Date: 7/28/24
+ * Time: 1:39 PM
+ */
+
+/**
+ * @description: Grant 100.00 overdraft funds.
+ * */
+class GoldOverdraft implements OverdraftInterface
+{
+    const OVERDRAFT_AMOUNT = 500 ;
+  public function isGrantOverdraftFunds(float $balance): bool{
+    return 0 <= (self::OVERDRAFT_AMOUNT + $balance);
+  }
+
+  public function getOverdraftFundsAmount() : float{
+    return self::OVERDRAFT_AMOUNT;
+  }
+
+}
